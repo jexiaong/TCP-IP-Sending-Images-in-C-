1a059cdae3d3bdc3a249a0c71c08928d5460eb0d
jexiaong <114371159+jexiaong@users.noreply.github.com>
2023-07-17 16:32:22 -0400

    Update sender.cpp

--- img-folder/sender.cpp
+++ img-folder/sender.cpp
@@ -3,26 +3,14 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <opencv2/opencv.hpp>
-#include <Windows.h>
+#include <filesystem>
+#include <windows.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
-void sendimage(const std::string& imagePath, SOCKET clientSocket)
-{
-    std::ifstream inputFile(imagePath, std::ios::binary);
-    char buffer[8192]; // Adjust buffer size as needed
-    do {
-        inputFile.read(buffer, sizeof(buffer));
-        int bytesRead = inputFile.gcount();
-        if (bytesRead > 0)
-            send(clientSocket, buffer, bytesRead, 0);
-    } while (!inputFile.eof());
-    inputFile.close();
-}
-
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        std::cerr << "Usage: sender <image_folder_path>" << std::endl;
+        std::cerr << "Usage: sender <image_folder_path>\n";
         return 1;
     }
 
@@ -32,14 +20,14 @@ int main(int argc, char* argv[]) {
     // Initialize Winsock
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cerr << "Failed to initialize Winsock" << std::endl;
+        std::cerr << "Failed to initialize Winsock\n";
         return 1;
     }
 
     // Create socket
     SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (clientSocket == INVALID_SOCKET) {
-        std::cerr << "Failed to create socket" << std::endl;
+        std::cerr << "Failed to create socket\n";
         WSACleanup();
         return 1;
     }
@@ -49,7 +37,7 @@ int main(int argc, char* argv[]) {
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(54000);
     if (inet_pton(AF_INET, serverIP, &(serverAddress.sin_addr)) != 1) {
-        std::cerr << "Invalid server IP address" << std::endl;
+        std::cerr << "Invalid server IP address\n";
         closesocket(clientSocket);
         WSACleanup();
         return 1;
@@ -57,23 +45,51 @@ int main(int argc, char* argv[]) {
 
     // Connect to the server
     if (connect(clientSocket, reinterpret_cast<SOCKADDR*>(&serverAddress), sizeof(serverAddress)) != 0) {
-        std::cerr << "Failed to connect to the server" << std::endl;
+        std::cerr << "Failed to connect to the server\n";
         closesocket(clientSocket);
         WSACleanup();
         return 1;
     }
+    
+    // Iterate over images in the folder
+    for (const auto& entry : std::filesystem::directory_iterator(folderpath)) {
 
-    // Read the image file
-    WIN32_FIND_DATAA findData;
-    HANDLE hFind = FindFirstFileA(folderpath.c_str(), &findData);
-    if (hFind != INVALID_HANDLE_VALUE) {
+        const std::string& imagePath = entry.path().string();
+
+        // Read the image file
+        std::ifstream file(imagePath, std::ios::binary | std::ios::ate);
+        if (!file.is_open()) {
+            std::cerr << "Failed to open image file: " << imagePath << std::endl;
+            closesocket(clientSocket);
+            WSACleanup();
+            return 1;
+        }
+
+        //Get the number of bytes of the image
+        uint32_t bytes = static_cast<uint32_t>(file.tellg());
+        std::cout << bytes << std::endl;
+        if (send(clientSocket, (char*)&bytes, sizeof(bytes), 0) == SOCKET_ERROR) {
+            std::cerr << "Failed to send bytes\n";
+            closesocket(clientSocket);
+            WSACleanup();
+            return 1;
+        }
+
+        // Load and send the image to the server
+        std::ifstream inputFile(imagePath, std::ios::binary);
+        char buffer[8192];
         do {
-            if (!(findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
-                std::string imagePath = folderpath + "\\" + findData.cFileName;
-                sendimage(imagePath, clientSocket);
+            inputFile.read(buffer, sizeof(buffer));
+            int bytesRead = inputFile.gcount();
+            if (bytesRead > 0) {
+                send(clientSocket, buffer, bytesRead, 0);
             }
-        } while (FindNextFileA(hFind, &findData));
-        FindClose(hFind);
+        } while (!inputFile.eof());
+        inputFile.close();
+
+        std::cout << "Image sent successfully: " << imagePath << std::endl;
+        
+        Sleep(10000);
     }
 
     // Clean up
